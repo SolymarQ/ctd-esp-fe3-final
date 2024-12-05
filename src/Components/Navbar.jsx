6a93c13b7355560642cca5a5011c7b729308562c
Solymar Quiaro <solymarquiaro@gmail.com>
2024-12-04 20:09:11 -0500

    Se ajustan los imports en los componentes de react router dom

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router'
+import { Link } from 'react-router-dom'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes } from "react-router-dom";
 import Contact from "./Contact";
 import Home from "./Home";
 import Detail from "./Detail";
