6497fdd450023f94a0cfde86ca2e7c1f30d37320
Solymar Quiaro <solymarquiaro@gmail.com>
2024-12-06 02:10:18 -0500

    Se agrega funcionalidad de pagina de detalle

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,11 +7,9 @@ const Navbar = () => {
 
   return (
     <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       <div>
         <Link to="/home">Home</Link>
         <Link to="/contact">Contact</Link>
-        <Link to="/dentist/:id">Detail</Link>
         <Link to="/favs">Favs</Link>
       </div>
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Detail = () => {
- 
   // Consumiendo el parametro dinamico de la URL deberan hacer un fetch a un user en especifico
 
-  return (
-    <>
-      <h1>Detail Dentist id </h1>
-      {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
-      {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
-    </>
-  )
-}
+  const [dentist, setDentist] = useState({});
+  // const params = useParams();
+  const { id } = useParams();
+  const url = "https://jsonplaceholder.typicode.com/users/" + id;
+  useEffect(() => {
+    axios(url).then((res) => {
+      setDentist(res.data);
+    });
+  }, []);
 
-export default Detail
+  return (
+    <div>
+      <header>
+        <h1>Detail Dentist {dentist.id} </h1>
+      </header>
+      <section className="detail">
+        <img src="/public/images/doctor.jpg" alt="Doctor" />
+        <div>
+          <h2>Nombre: {dentist.name}</h2>
+          <h3>Email: {dentist.email}</h3>
+          <h3>Telefono: {dentist.phone}</h3>
+          <h3>Website: {dentist.website}</h3>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Detail;
